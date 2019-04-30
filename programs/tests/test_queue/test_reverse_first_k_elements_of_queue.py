079dcd72497482218c49be48f1ce2409e373dd6e
ultimatecoder <jaysinhp@gmail.com>
2019-04-30 13:14:05 +0530

    Adding solution for Reverse K elements of Queue
    Adding a solution for reversing an expected elements of Queue.
    Program link: https://practice.geeksforgeeks.org/problems/queue-operations/1

---
+++ programs/tests/test_queue/test_reverse_first_k_elements_of_queue.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+
+import unittest
+
+from queue import reverse_first_k_elements_of_queue
+
+
+class TestQueue(unittest.TestCase):
+
+    def setUp(self):
+        self.queue = reverse_first_k_elements_of_queue.Queue()
+
+    def test_enqueue_with_one_element(self):
+        element = 4
+        self.queue.enqueue(element)
+        self.assertEqual(self.queue.front(), element)
+        self.assertEqual(self.queue.size(), 1)
+
+    def test_enqueue_with_multiple_elements(self):
+        elements = [1, 4, 5]
+        for element in elements:
+            with self.subTest(element=element):
+                self.queue.enqueue(element)
+
+        for element in elements:
+            with self.subTest(element=element):
+                self.assertEqual(self.queue.dequeue(), element)
+
+    def test_size(self):
+        self.queue.enqueue(3)
+        self.assertEqual(self.queue.size(), 1)
+
+        self.queue.enqueue(4)
+        self.assertEqual(self.queue.size(), 2)
+
+        self.queue.enqueue(6)
+        self.assertEqual(self.queue.size(), 3)
+
+        self.queue.dequeue()
+        self.assertEqual(self.queue.size(), 2)
+
+        self.queue.dequeue()
+        self.assertEqual(self.queue.size(), 1)
+
+    def test_dequeue_with_single_element(self):
+        element = 4
+        self.queue.enqueue(element)
+        self.assertEqual(self.queue.dequeue(), element)
+
+    def test_dequeue_with_multiple_elements(self):
+        elements = [1, 3, 5]
+
+        for element in elements:
+            self.queue.enqueue(element)
+
+        for element in elements:
+            self.assertEqual(self.queue.dequeue(), element)
+
+    def test_front_with_single_element(self):
+        element = 4
+        self.queue.enqueue(element)
+
+        self.assertEqual(self.queue.front(), element)
+
+    def test_front_with_multiple_elements(self):
+        elements = [1, 3, 4]
+
+        for element in elements:
+            self.queue.enqueue(element)
+            self.assertEqual(self.queue.front(), elements[0])
+
+    def test_reverse(self):
+        elements = list(range(1, 6))
+        expected_elements = elements[2::-1] + elements[3:]
+        for element in elements:
+            with self.subTest(element=element):
+                self.queue.enqueue(element)
+
+        self.queue.reverse(3)
+        for expected_element in expected_elements:
+            with self.subTest(element=element):
+                self.assertEqual(
+                    self.queue.dequeue(), expected_element
+                )
+
+
+
+class TestReverseElementsOfQueue(unittest.TestCase):
+
+    def test_validate_correct_values(self):
+        values_and_constraints = (
+            (3, lambda n: n >= 1),
+            (45, lambda n: (n >= 1) and (n <= 100))
+        )
+        for value, constraint in values_and_constraints:
+            with self.subTest(value=value):
+                self.assertTrue(
+                    reverse_first_k_elements_of_queue.validate(
+                        value, constraint
+                    )
+                )
+
+    def test_validate_wrong_values(self):
+        values_and_constraints = (
+            (3, lambda n: n <= 0),
+            (23, lambda n: (n >= 0) and (n <= 20)),
+        )
+        for value, constraint in values_and_constraints:
+            with self.subTest(value=value):
+                with self.assertRaises(ValueError):
+                    reverse_first_k_elements_of_queue.validate(
+                        value, constraint
+                    )
